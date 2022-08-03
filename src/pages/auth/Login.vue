938f59f29424e68c9630872b7ec50ea97ae53350
Matheus <dev.matheusnunes@gmail.com>
2022-08-03 16:17:12 -0300

    alteration of PageLogin

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,66 +1,73 @@
 <template>
-  <q-page padding>
-    <q-form class="row justify-center" @submit.prevent="handleLogin">
-      <p class="col-12 text-h6 text-center">Conecte-se</p>
-      <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
+  <q-page class="flex flex-center">
+    <q-form @submit.prevent="handleLogin">
+      <q-card style="max-width: 350px;" bordered>
+        <p class="text-h4 text-center q-mt-md">Conecte-se</p>
 
-        <q-input
-          label="EMail"
-          v-model="form.email"
-          type="email"
-          outlined
-          rounded
-          dense
-          lazy-rules
-          :rules="[val => (val && val.length > 0) || 'Por favor, insira seu email']"
-        >
-          <template v-slot:prepend>
-            <q-icon name="email" />
-          </template>
-        </q-input>
-
-        <q-input
-          label="Senha"
-          v-model="form.password"
-          type="password"
-          outlined
-          rounded
-          dense
-          lazy-rules
-          :rules="[val => (val && val.length >= 6) || 'Por favor, insira sua senha']"
-        >
-          <template v-slot:prepend>
-            <q-icon name="lock" />
-          </template>
-        </q-input>
-
-        <div class="q-pt-md">
-          <q-btn
-            class="full-width"
-            label="entrar"
-            color="positive"
+        <q-card-sections>
+          <q-input
+            class="q-mx-md q-mb-sm"
+            outlined
             rounded
+            placeholder="E-mail"
+            dense
+            v-model="form.email"
+            type="email"
+            lazy-rules
+            :rules="[val => (val && val.length > 0) || 'Por favor, digite seu e-email']"
+          >
+            <template v-slot:prepend>
+              <q-icon name="email" />
+            </template>
+          </q-input>
+
+          <q-input
+            class="q-mx-md q-mb-lg"
+            outlined
+            rounded
+            placeholder="Senha"
+            dense
+            v-model="form.password"
+            :type="isPwd ? 'password' : 'text'"
+            lazy-rules
+            :rules="[val => (val && val.length >= 6) || 'Por favor, digite sua senha']"
+          >
+            <template v-slot:prepend>
+              <q-icon name="lock" />
+            </template>
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+        </q-card-sections>
+
+        <q-card-sections class="flex flex-center">
+          <q-btn
+            class="q-btn q-mx-md"
+            rounded
+            dense
+            color="primary"
+            label="Entrar"
             type="submit"
+            style="width: 20vw;"
           />
           <q-btn
-            class="full-width"
-            label="recuperar senha"
-            color="black"
+            class="q-btn q-mx-md q-my-sm"
+            color="white"
             flat
-            :to="{ name:'forgot-password'}"
-          />
-        </div>
-        <div class="q-pt-md">
-          <q-btn
-            class="full-width"
-            label="criar nova conta"
-            color="blue-6"
             rounded
-            :to="{ name:'register' }"
+            dense
+            text-color="black"
+            label="Esqueci a Senha"
+            to="forgot-password"
           />
-        </div>
+        </q-card-sections>
 
-      </div>
+      </q-card>
     </q-form>
   </q-page>
 </template>
@@ -70,40 +77,39 @@ import { defineComponent, ref, onMounted } from 'vue'
 import useAuthUser from 'src/composables/UseAuthUser'
 import useNotify from 'src/composables/UseNotify'
 import { useRouter } from 'vue-router'
-
 export default defineComponent({
   name: 'PageLogin',
-
   setup () {
     const router = useRouter()
     const { login, isLoggedIn } = useAuthUser()
-    const { notifySuccess, notifyError } = useNotify()
-
+    const { notifyError, notifySuccess } = useNotify()
     const form = ref({
       email: '',
       password: ''
     })
-
     onMounted(() => {
       if (isLoggedIn) {
         router.push({ name: 'home' })
       }
     })
-
     const handleLogin = async () => {
       try {
         await login(form.value)
-        notifySuccess('Bem-vindo!!')
-        router.push({ name: 'home' })
+        notifySuccess('Olá, Bem-vindo')
+        router.replace({ name: 'home' })
       } catch (error) {
-        notifyError('Email ou senha inválida')
+        notifyError('E-mail ou senha inválidos')
       }
     }
-
     return {
       form,
+      isPwd: ref(true),
       handleLogin
     }
   }
 })
 </script>
+
+<style scoped>
+
+</style>
